0614ad75db6115351c0d141c0f12521c60f5b856
Alessandro Tulioli <alessandro.tulioli@totvs.com.br>
2023-01-16 20:49:00 -0300

    Cria Configurações na Startup

--- ControleFinanceiro.API/ControleFinanceiro.API/Startup.cs
+++ ControleFinanceiro.API/ControleFinanceiro.API/Startup.cs
@@ -1,16 +1,12 @@
+using ControleFinanceiro.BLL.Models;
+using ControleFinanceiro.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ControleFinanceiro.API
 {
@@ -26,13 +22,16 @@ namespace ControleFinanceiro.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Contexto>(opcoes => opcoes.UseSqlServer(Configuration.GetConnectionString("ConexaoBD")));
+
+            services.AddIdentity<Usuario, Funcao>().AddEntityFrameworkStores<Contexto>();
+
             services.AddCors();
 
             services.AddSpaStaticFiles(diretorio =>
             {
                 diretorio.RootPath = "ControleFinanceiro-UI";
             });
-
             services.AddControllers()
                 .AddJsonOptions(opcoes => 
                 {
